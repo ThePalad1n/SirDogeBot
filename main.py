a421a318cdd423d0333c3c2543fc154c9277d389
ThePalad1n <evangreen91@gmail.com>
2021-04-01 19:25:23 +0000

    added quotes

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 
 @client.event
 async def on_ready():
@@ -23,5 +31,9 @@ async def on_message(message):
 
     if message.content.startswith('/sir is brian gay?'):
         await message.channel.send('Yes')
+
+    if message.content.startswith('/sir inspire'):
+      quote = get_quote()
+      await message.channel.send(quote)
 
 client.run(os.getenv('TOKEN'))
