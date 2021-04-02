22452b5efad48941c893022e6abb0cdcf77f4713
ThePalad1n <>
2021-04-02 05:26:31 +0000

    added youtube capability but most commands dont work

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import os
 import requests
 import json
 import youtube_dl
+import asyncio
 from keep_alive import keep_alive
 
-client = discord.Client()
+#client = discord.Client()
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='sir ',intents=intents)
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -23,24 +28,122 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('/sir speak'):
+    if message.content.startswith('sir speak'):
         await message.channel.send('Hello M8!')
 
-    if message.content.startswith('/sir goodboi'):
+    if message.content.startswith('sir goodboi'):
         await message.channel.send('Ty!')
 
-    if message.content.startswith('/sir slur'):
+    if message.content.startswith('sir slur'):
         await message.channel.send('FuCk!')
 
-    if message.content.startswith('/sir inspire'):
+    if message.content.startswith('sir inspire'):
       quote = get_quote()
       await message.channel.send(quote)
 
-    if message.content.startswith('/sir news'):
+    if message.content.startswith('sir news'):
       await message.channel.send('Born on 4/1/2021, got a few cmds, but lookin to add memes soon type [/sir help] for the current command list')
 
-    if message.content.startswith('/sir help'):
+    if message.content.startswith('sir help'):
         await message.channel.send('You can type any of these for a cmd, [speak], [goodboi], [slur], [inspire], [news]')
 
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '172.18.0.1' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+        
+@bot.command(name='play', help='To play song')
+async def play(ctx,url):
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+    
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
 keep_alive()
+bot.run(os.getenv('TOKEN'))
 client.run(os.getenv('TOKEN'))
