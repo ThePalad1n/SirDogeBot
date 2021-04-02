5afcb70aff87e80c1df221faf36f3e22c34f6855
ThePalad1n <>
2021-04-02 23:50:02 +0000

    added weather feature with api call

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 import requests
 import json
+import math
 from keep_alive import keep_alive
 
 intents = discord.Intents().all()
@@ -56,6 +57,45 @@ async def news(ctx):
         await ctx.message.channel.send('Born on 4/1/2021, got a few cmds, but lookin to add memes soon. type [sir help] for the current command list.')
         return
 
+
+api_key = os.getenv('WEATHER_TOKEN')
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+#weather cmd
+@bot.command()
+async def weather(ctx, *, city: str):
+  city_name = city
+  complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+  response = requests.get(complete_url)
+  x = response.json()
+  channel = ctx.message.channel
+
+  if x["cod"] != "404":
+    async with channel.typing():
+
+        y = x["main"]
+        current_temperature = y["temp"]
+        current_temperature_fahrenheit = str(round(1.8*(current_temperature - 273.15)+32))
+        current_pressure = y["pressure"]
+        current_humidity = y["humidity"]
+        z = x["weather"]
+        weather_description = z[0]["description"]
+
+        weather_description = z[0]["description"]
+        embed = discord.Embed(title=f"Weather in {city_name}",
+                          color=ctx.guild.me.top_role.color,
+                          timestamp=ctx.message.created_at,)
+        embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+        embed.add_field(name="Temperature(F)", value=f"**{current_temperature_fahrenheit}°F**", inline=False)
+        embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+        embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+        embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
+
+    await channel.send(embed=embed)
+  else:
+      await channel.send("City not found.")
 
 
 keep_alive()
